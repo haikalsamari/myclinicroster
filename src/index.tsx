c54d2099fb05676c36fad979daa0a36c052d8d52
wsomad <87234180+haikalsamari@users.noreply.github.com>
2025-10-03 22:56:47 +0800

    why

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import CalendarContainer from "./features/calendar/components/calendar/CalendarC
 import Header from "./components/common/Header";
 import { useRef } from "react";
 import { usePrintToPDF } from "./hooks/usePrintToPDF";
+// import CalendarSideNote from "./features/calendar/components/calendar/CalendarSideNote";
 
 export default function IndexPage() {
     const calendarRef = useRef<HTMLDivElement>(null);
