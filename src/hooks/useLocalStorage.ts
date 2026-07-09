0c98d00cade01fcb7b1edc618b72d9df55e235ea
wsomad <87234180+haikalsamari@users.noreply.github.com>
2026-07-10 00:00:21 +0800

    Update packages to latest version

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from "react";
 
 import { storageService, type StorageKey } from "@/services/rosterService";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 // export function useLocalStorage<T>(key: string, initialValue: T) {
 //     const [storedValue, setStoredValue] = useState<T | null>(null);
@@ -33,18 +33,13 @@ import { useEffect, useState } from "react";
 // }
 
 export function useLocalStorage<T>(key: StorageKey, initialValue: T) {
-    const [storedValue, setStoredValue] = useState<T | null>(null);
-
-    useEffect(() => {
-        const value = storageService.load<T>(key) ?? initialValue;
-        setStoredValue(value);
-    }, [key]);
+    const [storedValue, setStoredValue] = useState<T>(() => storageService.load<T>(key) ?? initialValue);
 
     const setValue = (value: T) => {
         setStoredValue(value);
         storageService.save(key, value);
     }
 
-    return [storedValue ?? initialValue, setValue] as const;
+    return [storedValue, setValue] as const;
 }
 
